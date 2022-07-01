5ee7994227e8eba574ca00ef2d7189688d1d51f6
akash02ita-raspberry <akash02ita@gmail.com>
2022-07-01 08:21:00 -0600

    Completed src code Also fixed typo in main.h regarding BAR_a_PIN_NUM macro which is defined to 6 and not 7

---
+++ 0/section Raspberry Pi/src/main.c
@@ -0,0 +1,228 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+#include <src/mylib/initGPIO.h>
+#include <src/mylib/initGPIO.c>
+#include <src/mylib/my_gpio_assist.h>
+#include <src/mylib/my_gpio_assist.c>
+
+#include <src/main.h>
+
+void initialize_gpio_pins()
+{
+    // input mode
+    Init_GPIO(LEFT_BUTTON_PIN_NUM, 0);
+    Init_GPIO(RIGHT_BUTTON_PIN_NUM, 0);
+
+    // output mode
+    Init_GPIO(BAR_a_PIN_NUM, 1);
+    Init_GPIO(BAR_b_PIN_NUM, 1);
+    Init_GPIO(BAR_c_PIN_NUM, 1);
+    Init_GPIO(BAR_d_PIN_NUM, 1);
+    Init_GPIO(BAR_e_PIN_NUM, 1);
+    Init_GPIO(BAR_f_PIN_NUM, 1);
+    Init_GPIO(BAR_g_PIN_NUM, 1);
+}
+
+void output_digit(const int digit)
+{
+    switch(digit)
+    {
+        case 0:
+        {
+            write_pin(BAR_a_PIN_NUM, 1);
+            write_pin(BAR_b_PIN_NUM, 1);
+            write_pin(BAR_c_PIN_NUM, 1);
+            write_pin(BAR_d_PIN_NUM, 1);
+            write_pin(BAR_e_PIN_NUM, 1);
+            write_pin(BAR_f_PIN_NUM, 0);
+            write_pin(BAR_g_PIN_NUM, 1);
+            break;
+        }
+        case 1:
+        {
+            write_pin(BAR_a_PIN_NUM, 0);
+            write_pin(BAR_b_PIN_NUM, 0);
+            write_pin(BAR_c_PIN_NUM, 1);
+            write_pin(BAR_d_PIN_NUM, 1);
+            write_pin(BAR_e_PIN_NUM, 0);
+            write_pin(BAR_f_PIN_NUM, 0);
+            write_pin(BAR_g_PIN_NUM, 0);
+            break;
+        }
+        case 2:
+        {
+            write_pin(BAR_a_PIN_NUM, 0);
+            write_pin(BAR_b_PIN_NUM, 1);
+            write_pin(BAR_c_PIN_NUM, 1);
+            write_pin(BAR_d_PIN_NUM, 0);
+            write_pin(BAR_e_PIN_NUM, 1);
+            write_pin(BAR_f_PIN_NUM, 1);
+            write_pin(BAR_g_PIN_NUM, 1);
+            break;
+        }
+        case 3:
+        {
+            write_pin(BAR_a_PIN_NUM, 0);
+            write_pin(BAR_b_PIN_NUM, 0);
+            write_pin(BAR_c_PIN_NUM, 1);
+            write_pin(BAR_d_PIN_NUM, 1);
+            write_pin(BAR_e_PIN_NUM, 1);
+            write_pin(BAR_f_PIN_NUM, 1);
+            write_pin(BAR_g_PIN_NUM, 1);
+            break;
+        }
+        case 4:
+        {
+            write_pin(BAR_a_PIN_NUM, 1);
+            write_pin(BAR_b_PIN_NUM, 0);
+            write_pin(BAR_c_PIN_NUM, 1);
+            write_pin(BAR_d_PIN_NUM, 1);
+            write_pin(BAR_e_PIN_NUM, 0);
+            write_pin(BAR_f_PIN_NUM, 1);
+            write_pin(BAR_g_PIN_NUM, 0);
+            break;
+        }
+        case 5:
+        {
+            write_pin(BAR_a_PIN_NUM, 1);
+            write_pin(BAR_b_PIN_NUM, 0);
+            write_pin(BAR_c_PIN_NUM, 0);
+            write_pin(BAR_d_PIN_NUM, 1);
+            write_pin(BAR_e_PIN_NUM, 1);
+            write_pin(BAR_f_PIN_NUM, 1);
+            write_pin(BAR_g_PIN_NUM, 1);
+            break;
+        }
+        case 6:
+        {
+            write_pin(BAR_a_PIN_NUM, 1);
+            write_pin(BAR_b_PIN_NUM, 1);
+            write_pin(BAR_c_PIN_NUM, 0);
+            write_pin(BAR_d_PIN_NUM, 1);
+            write_pin(BAR_e_PIN_NUM, 1);
+            write_pin(BAR_f_PIN_NUM, 1);
+            write_pin(BAR_g_PIN_NUM, 1);
+            break;
+        }
+        case 7:
+        {
+            write_pin(BAR_a_PIN_NUM, 0);
+            write_pin(BAR_b_PIN_NUM, 0);
+            write_pin(BAR_c_PIN_NUM, 1);
+            write_pin(BAR_d_PIN_NUM, 1);
+            write_pin(BAR_e_PIN_NUM, 1);
+            write_pin(BAR_f_PIN_NUM, 0);
+            write_pin(BAR_g_PIN_NUM, 0);
+            break;
+        }
+        case 8:
+        {
+            write_pin(BAR_a_PIN_NUM, 1);
+            write_pin(BAR_b_PIN_NUM, 1);
+            write_pin(BAR_c_PIN_NUM, 1);
+            write_pin(BAR_d_PIN_NUM, 1);
+            write_pin(BAR_e_PIN_NUM, 1);
+            write_pin(BAR_f_PIN_NUM, 1);
+            write_pin(BAR_g_PIN_NUM, 1);
+            break;
+        }
+        case 9:
+        {
+            write_pin(BAR_a_PIN_NUM, 1);
+            write_pin(BAR_b_PIN_NUM, 0);
+            write_pin(BAR_c_PIN_NUM, 1);
+            write_pin(BAR_d_PIN_NUM, 1);
+            write_pin(BAR_e_PIN_NUM, 1);
+            write_pin(BAR_f_PIN_NUM, 1);
+            write_pin(BAR_g_PIN_NUM, 1);
+            break;
+        }
+        default:
+        {
+            printf("Error invalid digit %d\n", digit);
+            exit(-1);
+            break;
+        }
+    }
+}
+
+void turn_off()
+{
+    write_pin(BAR_a_PIN_NUM, 0);
+    write_pin(BAR_b_PIN_NUM, 0);
+    write_pin(BAR_c_PIN_NUM, 0);
+    write_pin(BAR_d_PIN_NUM, 0);
+    write_pin(BAR_e_PIN_NUM, 0);
+    write_pin(BAR_f_PIN_NUM, 0);
+    write_pin(BAR_g_PIN_NUM, 0);
+    sleep(1);
+}
+int update_digit(int *digit, int *left_pressed, int *right_pressed)
+{
+    /* note: keeping track of button pressed status does not really help as probably the signal from button to gpio reading pin is not always continuosly on when button is pressed
+    Although it somewhat helps, with use of usleep() function, to have too much speed in changing values */
+
+    // decrement
+    if (read_pin(LEFT_BUTTON_PIN_NUM) && *digit > 0 && !(*left_pressed))
+    {
+        *left_pressed = 1;
+        (*digit)--;
+        return 1;
+    }
+    else
+        *left_pressed = 0;
+
+    // increment
+    if (read_pin(RIGHT_BUTTON_PIN_NUM) && *digit < 9 && !(*right_pressed))
+    {
+        *right_pressed = 1;
+        (*digit)++;
+        return 1;
+    }
+    else
+        *right_pressed = 0;
+
+    return 0;
+}
+
+
+void * thread_digit_displayer(void *arg)
+{
+    // display the digit that can be modified interactive until the thread is not cancelled
+    printf("To halt the program press enter on the terminal\n.");
+    printf("\nWELCOME!\nHere the simple way to interact with the breadboard:\n\t[left button]: decrement digit if greater than 0\n\t[right button]: increment digit less than 9\n");
+    int digit = 0; // start
+    int left_pressed = 0;
+    int right_pressed = 0;
+    output_digit(digit);
+    while (1)
+    {
+        // only write to pin (so do fewer gpio memory calls) when a new digit is there
+        if (update_digit(&digit, &left_pressed, &right_pressed))
+                output_digit(digit);
+        usleep(100000);
+    }
+}
+
+
+int main()
+{
+    // initialize gpio MEMORY address exactly once
+    init_base_gpio();
+    // initialize the pins
+    initialize_gpio_pins();
+
+    pthread_t digit_displayer_thread;
+    pthread_create(&digit_displayer_thread, NULL, thread_digit_displayer, NULL);
+
+    // keep running the thread until user presses enter on the terminal
+    getchar();
+    int status  = pthread_cancel(digit_displayer_thread);
+    // sleep(1);
+    printf("The status of cancel is %d\n", status);
+    turn_off();
+    return 0;
+}
--- 0/section Raspberry Pi/src/main.h
+++ 0/section Raspberry Pi/src/main.h
@@ -7,10 +7,66 @@
 #define RIGHT_BUTTON_PIN_NUM 27
 
 // output pins
-#define BAR_a_PIN_NUM 7
+#define BAR_a_PIN_NUM 6
 #define BAR_b_PIN_NUM 13
 #define BAR_c_PIN_NUM 12
 #define BAR_d_PIN_NUM 16
 #define BAR_e_PIN_NUM 19
 #define BAR_f_PIN_NUM 20
 #define BAR_g_PIN_NUM 26
+
+/*
+
+NOTE: every digit is formatted in 7 bars as follows:
+
+       e
+     ----
+   a|    |c
+    |  f |
+     ----
+   b|    |d
+    |    |
+     ----
+       g
+*/
+
+
+/**
+ * @brief initializes all pins defined in above macros
+ * 
+ */
+void initialize_gpio_pins();
+
+
+/**
+ * @brief uses above macros to display the digit in the format specified above
+ * 
+ * @param digit must be between 0-9, inclusively
+ */
+void output_digit(const int digit);
+
+
+/**
+ * @brief if left or right button is pressed decrements or increments digit if it can stay between 0-9, inclusively
+ * 
+ * @param digit 
+ * @param left_pressed 
+ * @param right_pressed 
+ * @return int [0: no updates/changes, 1: updates/changes occurred] 
+ */
+int update_digit(int *digit, int *left_pressed, int *right_pressed);
+
+
+/**
+ * @brief thread function that runs forever, unless cancelled by main thread.
+ * Updates digit and displays on the output pins
+ * 
+ */
+void * thread_digit_displayer(void *arg);
+
+
+/**
+ * @brief Function for the main thread.
+ * 
+ */
+int main();
